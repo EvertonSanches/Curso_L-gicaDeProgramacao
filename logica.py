893f4ab19e5384c97c9defc964d6b13f922bf1ea
Everton Sanches <74841165+EvertonSanches@users.noreply.github.com>
2026-05-13 16:16:35 -0300

    criado arquivo logica para exercitar

---
+++ logica.py
@@ -0,0 +1,52 @@
+# contador = 0
+
+# for saudacao in range(5):
+#     print("Olá")
+
+
+# for numero in range(1, 11):
+#     print(numero)
+
+# for numero in range(10, 0, -1):
+#     print(numero)
+
+# for numero in range(2, 21, 2):
+#     print(f'Numeros pares até 20: {numero}')
+
+# for numero in range(1, 16, 2):
+#     print(f'Numeros impares de 1 a 15: {numero}')
+
+# for i in range(5):
+#     numero = int(input("Digite um numero: "))
+#     print(f"Voce digitou o numero: {numero}")
+
+# for i in range(3):
+#     nome = input("Digite seu nome: ")
+#     print(f"Olá, {nome}!")
+
+# for i in range(4):
+#     nota = float(input("Digite a nota do aluno: "))
+#     print(f"A nota do aluno é: {nota}")
+
+# tabuada = int(input("Digite um numero para ver sua tabuada: "))
+# for t in range(1, 11):
+#     resultado = tabuada * t
+#     print(f"{tabuada} x {t} = {resultado}")
+
+# contador = 0
+
+# for n in range(5):
+#     numero = int(input("Digite um numero: "))
+#     if numero > 0:
+#         contador += 1
+# print(f"Voce digitou {contador} numeros positivos.")
+
+#soma dos numeros digitados
+# contador = 0
+
+# for n in range(5):
+#     numero = int(input("Digite um numero: "))
+#     contador = contador + numero
+
+# print(f"A soma dos numeros digitados é: {contador}")
+
